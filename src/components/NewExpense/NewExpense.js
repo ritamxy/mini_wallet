b4a14f018eaac8d3e1961404c4c31cd02502bd52
ritamxy <58613546+ritamxy@users.noreply.github.com>
2022-04-08 22:48:02 +1000

    add cancel button to input form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,22 +3,32 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
-  const [isEditing, setIsEditing] = useState(false);
 
   const starEditingHandler = () => {
     setIsEditing(true);
   };
 
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
   return (
     <div className="new-expense">
-      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
       {!isEditing && (
         <button onClick={starEditingHandler}>Add New Expense</button>
       )}
